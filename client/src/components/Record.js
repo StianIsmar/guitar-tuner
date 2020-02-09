246eaa6da12b1e465330cd8289e327e8325e327f
Stian <stian.ismar@gmail.com>
2020-02-09 13:16:28 +0100

    Changed CSS

--- client/src/components/Record.js
+++ client/src/components/Record.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Fragment } from 'react';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/Button';
 import MicRecorder from 'mic-recorder-to-mp3';
 import TransmitAudio from './TransmitAudio'
+import "../css/Record.css"
 
 const Mp3Recorder = new MicRecorder({ bitRate: 128 });
 
@@ -27,6 +29,10 @@ class Record extends Component {
                 .start()
                 .then(() => {
                     this.setState({ isRecording: true });
+                    setTimeout(() => {
+                        this.stop();
+                    }, 2000);
+
                 }).catch((e) => console.error(e));
         }
     };
@@ -58,12 +64,36 @@ class Record extends Component {
 
     render() {
         return (
-            <div className="Record">
-                <Button variant="danger" onClick={this.start} disabled={this.state.isRecording}>Record</Button>
-                <Button variant="danger" onClick={this.stop} disabled={!this.state.isRecording}>Stop</Button>
-                <audio src={this.state.blobURL} controls="controls" />
-                <TransmitAudio ref={this.transmitAudioElement}/>
-            </div>
+            <div className="record-wrapper row">
+                <header>
+                    <h1>
+                        Welcome to GuitarTuner.
+                    </h1>
+                    <h3>Select the string you are tuning</h3>
+                </header>
+
+
+                <div className="select-string row">
+                    <ButtonGroup className="mb-3" aria-label="Basic example">
+                        <Button variant="secondary">E<sub>2</sub></Button>
+                        <Button variant="secondary">A</Button>
+                        <Button variant="secondary">D</Button>
+                        <Button variant="secondary">G</Button>
+                        <Button variant="secondary">B</Button>
+                        <Button variant="secondary">E<sub>4</sub></Button>
+
+
+                    </ButtonGroup>
+                </div>
+                <div className="record-button row">
+                    <Button variant="danger" onClick={this.start} disabled={this.state.isRecording}>Record</Button>
+                    <Button variant="danger" onClick={this.stop} disabled={!this.state.isRecording}>Stop</Button>
+                </div>
+                <div className="playback row">
+                    <audio src={this.state.blobURL} controls="controls" />
+                </div>
+                <TransmitAudio ref={this.transmitAudioElement} />
+            </div >
 
         );
     }
